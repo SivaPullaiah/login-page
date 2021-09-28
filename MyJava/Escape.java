c028ad005cafd1269d3d17c1cd2e52598da55ddf
Ravula Govardhan <77828834+lecturesbyravulagovardhan@users.noreply.github.com>
2021-09-28 21:30:15 +0530

    Add files via upload

---
+++ MyJava/Escape.java
@@ -0,0 +1,24 @@
+class Escape
+{
+	public static void main(String args[])
+	{
+		
+		// Add a tab between Ravula and Govardhan
+		System.out.println("Ravula\tGovardhan");
+		
+		// Add a new line after Ravula
+		System.out.println("Ravula\nGovardhan");
+		
+		// Add a carriage return after Ravula
+		System.out.println("Ravula\rGovardhan");
+		
+		// Add a single quote between Ravula and Govardhan
+		System.out.println("Ravula\'Govardhan");
+		
+		// Add a double quote between Ravula and Govardhan
+		System.out.println("Ravula\"Govardhan");
+		
+		// Add a backslash between Ravula and Govardhan
+		System.out.println("Ravula\\Govardhan");
+	}
+}
