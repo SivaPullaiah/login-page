bf86d3d4213e3f899e3a119f3468450c56bc3e50
Ravula Govardhan <77828834+lecturesbyravulagovardhan@users.noreply.github.com>
2021-10-05 11:56:47 +0530

    Add files via upload

---
+++ MyJava/Bitwise.java
@@ -0,0 +1,33 @@
+class Bitwise
+{
+	public static void main(String[] args)
+	{
+		int a = 5;
+		/* int b = 7;
+		
+		// Bitwise AND
+		System.out.println("a&b=" + (a&b));
+
+		// Bitwise OR
+		System.out.println("a|b=" + (a|b));
+
+		// Bitwise XOR
+		System.out.println("a^b="+(a^b)); */
+		
+		// Bitwise Complement
+		System.out.println("~a=" + ~a);
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
