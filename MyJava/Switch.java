6c7a3e3db659730616bc0edcd33e7e8538bde6c0
Ravula Govardhan <77828834+lecturesbyravulagovardhan@users.noreply.github.com>
2021-11-29 20:54:24 +0530

    Add files via upload

---
+++ MyJava/Switch.java
@@ -0,0 +1,17 @@
+class Switch
+{
+	public static void main(String[] args)
+	{
+		int ch = 3;
+		switch( ch )
+		{
+			case 1: System.out.println("First choice");
+					break;
+					
+			case 2: System.out.println("Second choice");
+					break;
+					
+			default: System.out.println("Wrong choice");
+		}
+	}
+}
